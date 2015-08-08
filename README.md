d706f1f969a7cf52ed16125c11f27e6a0d928a53
Octav Sandulescu <github@narc.ro>
2015-08-08 11:12:13 +0000

    Initial commit.
    Fetches Factorio update packages, stable or experimental, or dies trying
    -- as long as you have your updater username and token handy.

---
+++ README.md
@@ -0,0 +1,87 @@
+This is an aid for [Factorio](http://www.factorio.com/) headless server owners.
+It fetches update packages from factorio.com for you, so that you can then apply
+the update with minimal downtime.
+
+
+## Usage ##
+
+This is a Python 2 script using a single non-standard library,
+[Requests](http://requests.readthedocs.org/en/latest/).
+
+To install the required dependency, you should need do no more than run `pip
+install requests` (or, in case of emergency, `easy_install requests`). If this
+does not work, you are encouraged to read the linked documentation and try to
+figure out what's gone wrong.
+
+---
+
+For the script to successfully connect to the Factorio updater API, you will
+need two key pieces of information: your updater username and API token. Both
+of these can be obtained from a working client copy of Factorio (like, say, the
+one you're using to play on your headless server).
+
+Look in your Factorio folder (e.g., `C:\Games\Factorio`), and find (and open)
+the file named `player-data.json`. Near the end of the file there will be some
+lines that look like this:
+
+```JSON
+    "updater-username": "Narc",
+    "updater-token": "xyz123abc456def789ghijklmnopqr"
+```
+
+You will want to keep both of these for use with the update script.
+
+---
+
+From there, it's really simple: open a shell on the machine running your
+headless server, fetch the updater script, and run it. Here's an example
+session:
+
+```
+[narc@odin ~/src/factorio-updater]% python update_factorio.py --help
+usage: update_factorio.py [-h] [-d] [-u USER] [-t TOKEN]
+                          [-p {core-linux32,core-linux64,core-mac,core-win32,core-win64}]
+                          [-f FOR_VERSION] [-O OUTPUT_PATH] [-x]
+
+Fetches Factorio update packages (e.g., for headless servers)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d, --dry-run         Don't download files, just state which updates would
+                        be downloaded.
+  -u USER, --user USER  Your Factorio updater username, from player-data.json.
+  -t TOKEN, --token TOKEN
+                        Your Factorio updater token, also from player-
+                        data.json.
+  -p {core-linux32,core-linux64,core-mac,core-win32,core-win64}, --package {core-linux32,core-linux64,core-mac,core-win32,core-win64}
+                        Which Factorio package to look for updates for, e.g.,
+                        core-linux64 for a 64-bit Linux Factorio.
+  -f FOR_VERSION, --for-version FOR_VERSION
+                        Which Factorio version you currently have, e.g.,
+                        0.12.2.
+  -O OUTPUT_PATH, --output-path OUTPUT_PATH
+                        Where to put downloaded files.
+  -x, --experimental    Download experimental versions, too (otherwise only
+                        stable updates are considered).
+[narc@odin ~/src/factorio-updater]% python update_factorio.py -u Narc -t xyz123abc456def789ghijklmnopqr -p core-linux64 -f 0.12.2 -x
+Wrote /tmp/core-linux64-0.12.2-0.12.3-update.zip, apply with `factorio --apply-update /tmp/core-linux64-0.12.2-0.12.3-update.zip`
+[narc@odin ~/src/factorio-updater]% cd ~/srv/factorio/bin/x64
+[narc@odin ~/srv/factorio/bin/x64]% ./factorio --apply-update /tmp/core-linux64-0.12.2-0.12.3-update.zip
+[...stuff happens...]
+[narc@odin ~/srv/factorio/bin/x64]% rm /tmp/core-linux64-0.12.2-0.12.3-update.zip
+```
+
+
+
+## License ##
+
+The source of **Factorio Update Helperr** is Copyright 2015 Octav "narc" Sandulescu. It
+is licensed under the [MIT license][mit], available in this package in the file
+[LICENSE.md](LICENSE.md).
+
+[mit]: http://opensource.org/licenses/mit-license.html
+
+
+## Statistics ##
+
+1 API key was invalidated during the development of this script.
---
+++ update_factorio.py
@@ -0,0 +1,119 @@
+import os, posixpath, requests, re, urlparse
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Fetches Factorio update packages (e.g., for headless servers)")
+parser.add_argument('-d', '--dry-run', action='store_true', dest='dry_run',
+                    help="Don't download files, just state which updates would be downloaded.")
+parser.add_argument('-u', '--user',
+                    help="Your Factorio updater username, from player-data.json.")
+parser.add_argument('-t', '--token',
+                    help="Your Factorio updater token, also from player-data.json.")
+parser.add_argument('-p', '--package', default='core-linux64',
+                    choices=['core-linux32', 'core-linux64', 'core-mac', 'core-win32', 'core-win64'],
+                    help="Which Factorio package to look for updates for, "
+                    "e.g., core-linux64 for a 64-bit Linux Factorio.")
+parser.add_argument('-f', '--for-version',
+                    help="Which Factorio version you currently have, e.g., 0.12.2.")
+parser.add_argument('-O', '--output-path', default='/tmp',
+                    help="Where to put downloaded files.")
+parser.add_argument('-x', '--experimental', action='store_true', dest='experimental',
+                    help="Download experimental versions, too (otherwise only stable updates are considered).")
+
+
+class DownloadFailed(Exception): pass
+
+
+def version_key(v):
+    if v is None:
+        return []
+    return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+
+
+def get_updater_data(user, updater_token):
+    payload = {'username': user, 'token': updater_token, 'apiVersion': 2}
+    r = requests.get('https://www.factorio.com/updater/get-available-versions', params=payload)
+    if r.status_code != 200:
+        raise DownloadFailed('Could not download version list.', r.status_code)
+    return r.json()
+
+
+def pick_updates(updater_json, factorio_package, from_version, experimental=False):
+    latest = [None, None]
+    available_updates = {}
+    for row in updater_json[factorio_package]:
+        if 'from' not in row:
+            latest[0] = row['stable']
+            continue
+        available_updates[row['from']] = row['to']
+
+        latest[1] = max(latest[1], row['to'], key=version_key)
+
+    updates = []
+
+    current_version = from_version
+    while current_version in available_updates:
+        new_version = available_updates[current_version]
+        if not experimental and max(current_version, latest[0], key=version_key) == current_version:
+            break
+
+        updates.append({'from': current_version, 'to': new_version})
+        current_version = new_version
+
+    return updates, latest
+
+
+def get_update_link(username, token, package, update):
+    payload = {'username': username,
+               'token': token,
+               'package': package,
+               'from': update['from'],
+               'to': update['to'],
+               'apiVersion': 2}
+    r = requests.get('https://www.factorio.com/updater/get-download-link', params=payload)
+    if r.status_code != 200:
+        raise DownloadFailed('Could not obtain download link.', r.status_code, update)
+    return r.json()[0]
+
+
+def fetch_update(output_path, url):
+    fname = posixpath.basename(urlparse.urlsplit(url).path)
+    fpath = os.path.join(output_path, fname)
+    r = requests.get(url, stream=True, verify=False)
+    with open(fpath, 'wb') as fd:
+        for chunk in r.iter_content(8192):
+            fd.write(chunk)
+
+    print('Wrote %(fpath)s, apply with `factorio --apply-update %(fpath)s`' % {'fpath': fpath})
+
+
+def main():
+    args = parser.parse_args()
+
+    j = get_updater_data(args.user, args.token)
+    updates, latest = pick_updates(j, args.package, args.for_version, args.experimental)
+
+    if not updates:
+        message = 'No updates available for version %s' % args.for_version
+        if not args.experimental:
+            if latest[0]:
+                message += ' (latest stable is %s).' % latest[0]
+            else:
+                message += '.'
+            message += ' Did you want `--experimental`?'
+        else:
+            message += ' (latest experimental is %s).' % latest[1]
+        print(message)
+        return 1
+
+    for u in updates:
+        if args.dry_run:
+            print 'Dry run: would have fetched update from %s to %s.' % (u['from'], u['to'])
+        else:
+            url = get_update_link(args.user, args.token, args.package, u)
+            if url is not None:
+                fetch_update(args.output_path, url)
+
+
+if __name__ == '__main__':
+    main()
